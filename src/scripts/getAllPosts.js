f1f478a43e05ec40f3de8f685f00ed0ff7b19558
Goran Jovic <goran1010jovic@gmail.com>
2025-07-15 18:11:23 +0200

    Change fetch link

--- src/scripts/getAllPosts.js
+++ src/scripts/getAllPosts.js
@@ -1,11 +1,16 @@
 const VITE_URL = import.meta.env.VITE_URL || "http://localhost:3000";
 
+console.log(VITE_URL);
+
 export default async function getAllPosts() {
-  const response = await fetch(`${VITE_URL}/api/posts`, {
-    mode: "cors",
-    method: "GET",
-    headers: { "Content-Type": "application/json" },
-  });
+  const response = await fetch(
+    `http://dry-leona-goran-jovic-1010-5d98d993.koyeb.app/api/posts`,
+    {
+      mode: "cors",
+      method: "GET",
+      headers: { "Content-Type": "application/json" },
+    }
+  );
   const result = await response.json();
   if (response.ok) {
     return result;
